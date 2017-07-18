3054b528f60f1a9de2cb1df18dbcf8c43434a017
Robert Wilde <wilde4@gmail.com>
2017-07-18 10:33:01 +0100

    Update Default for Live

--- opsworks_clockwork/recipes/default.rb
+++ opsworks_clockwork/recipes/default.rb
@@ -14,8 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-opsworks_clockwork "oliver-clockwork" do
-  working_directory "/srv/www/oliver/current"
+opsworks_clockwork "live-clockwork" do
+  working_directory "/srv/www/live/current"
   rails_env "production"
   user "deploy"
   group "www-data"
