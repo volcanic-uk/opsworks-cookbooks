a1c3f7d959d1c484096349285bba2e5c1411a5f2
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2018-07-10 17:49:38 +0100

    fix block notation

--- fonts/recipes/zawgyi.rb
+++ fonts/recipes/zawgyi.rb
@@ -1,8 +1,15 @@
+directory '/usr/share/font' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
 execute "download font" do
   command 'wget https://s3.amazonaws.com/oliver-dev/fonts/zawgyi.ttf'
   user "root"
   cwd '/usr/share/font/'
-  not_if File.exists?('/usr/share/font/zawgyi.ttf')
+  not_if { ::File.exists?('/usr/share/font/zawgyi.ttf') }
 end
 
 execute "refresh font cache" do
