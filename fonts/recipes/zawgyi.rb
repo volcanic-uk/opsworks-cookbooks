0f914c1af6bf46e7374ba033f035ec1213e42c9a
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2018-07-10 12:09:47 +0100

    dont run if font already exists

--- fonts/recipes/zawgyi.rb
+++ fonts/recipes/zawgyi.rb
@@ -1,6 +1,7 @@
 execute "download font" do
   command 'wget -O /usr/share/font/zawgyi.ttf https://s3.amazonaws.com/oliver-dev/fonts/zawgyi.ttf'
   user "root"
+  not_if File.exists?('/usr/share/font/zawgyi.ttf')
 end
 
 execute "refresh font cache" do
