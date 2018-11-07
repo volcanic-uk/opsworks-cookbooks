9af2af39f2ac4c4248ceaa679f3dacec24635293
Andy Turner <a.turner@volcanic.co.uk>
2018-11-07 16:21:12 +0000

    Update shoryuken recipe to setup multiple upstart services

--- opsworks_shoryuken/recipes/deploy.rb
+++ opsworks_shoryuken/recipes/deploy.rb
@@ -3,6 +3,7 @@ node[:deploy].each do |application, deploy|
     shoryuken_config = deploy['shoryuken']
     release_path = ::File.join(deploy[:deploy_to], 'current')
     rails_env = deploy[:rails_env]
+    num_workers = shoryuken_config['num_workers'] || 2
     start_command = shoryuken_config['start_command'] || "bundle exec shoryuken -R -C config/shoryuken.yml 2>&1 >> log/shoryuken.log"
     env = deploy['environment_variables'] || {}
 
@@ -23,7 +24,24 @@ node[:deploy].each do |application, deploy|
       })
     end
 
+    template "setup shoryuken-workers.conf" do
+      path "/etc/init/shoryuken-workers-#{application}.conf"
+      source "shoryuken-workers.conf.erb"
+      owner "root"
+      group "root"
+      mode 0644
+      variables({
+        app_name: application,
+        num_workers: num_workers,
+      })
+    end
+
     service "shoryuken-#{application}" do
+      provider Chef::Provider::Service::Upstart
+      supports stop: true, start: true, restart: true, status: true
+    end
+
+    service "shoryuken-workers-#{application}" do
       provider Chef::Provider::Service::Upstart
       supports stop: true, start: true, restart: true, status: true
     end
@@ -31,7 +49,7 @@ node[:deploy].each do |application, deploy|
     # always restart shoryuken on deploy since we assume the code must need to be reloaded
     bash 'restart_shoryuken' do
       code "echo noop"
-      notifies :restart, "service[shoryuken-#{application}]"
+      notifies :restart, "service[shoryuken-workers-#{application}]"
     end
   end
 end
