3e2939d8623768728b385dfbfaa01da52e1b2df9
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2020-11-25 14:10:07 +0000

    add support for extra ips

--- varnish/recipes/write_varnish_config.rb
+++ varnish/recipes/write_varnish_config.rb
@@ -1,9 +1,12 @@
+extra_ips = node[:varnish].map{|e| {:private_ip => e} }
+
 template "/etc/varnish/backends.vcl" do
   mode '0755'
   owner 'root'
   # group 
   source "backends.vcl.erb"
   variables(:servers => search(:node, "layers:public"), :hp_servers => search(:node, 'layers:high_performance_private'))
+  # variables(:servers => search(:node, "layers:public"), :hp_servers => (search(:node, 'layers:high_performance_private').to_a + extra_ips))
 end
 
 template "/etc/varnish/custom.vcl" do
